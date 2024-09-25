a51a492b23a36369250e75178699a19950a958f7
Abdalrhman-Jabri <abdalrhmanjabri08@gmail.com>
2024-09-25 11:38:14 +0300

    Second attempt

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,8 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use App\Observers\ProjectObserver;
 
+#[ObservedBy([ProjectObserver::class])]
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
 }
